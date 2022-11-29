916e96131917bbab8397658e3db14057dc8c3db9
jgozalez <camilogon.99@gmail.com>
2022-11-28 22:13:33 -0500

    Se agrego un metodo POST

--- Practica_02/src/app.controller.ts
+++ Practica_02/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+   private persona = "Mundo";
+
+   @Get()
+   getHello(): string {
+      return `Hola: ${this.persona}`
+   }
+
+   @Post(':nombre')
+   modificar(@Param('nombre') nombre: string): string {
+      this.persona = nombre;
+      return `Mensaje modificado: ${this.persona}`
+   }
 }
